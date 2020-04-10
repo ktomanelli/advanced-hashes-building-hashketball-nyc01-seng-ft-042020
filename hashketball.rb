5cf6d2c76e7a97c3c1fee58c7a60a19a96efe614
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-10 01:44:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,7 +65,7 @@ def game_hash
     :colors =>["Turquoise","Purple"],
     :players =>[
       {
-          :player_name => "",
+          :player_name => "Jeff Adrien",
           :number => "",
           :shoe => "",
           :points => "",
